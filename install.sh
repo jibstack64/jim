80a57d71b0e620e38978f05d874845f9e9ce39ca
jibstack64 <jibbystar@proton.me>
2022-12-19 17:19:37 +0000

    Final fix?

--- install.sh
+++ install.sh
@@ -1,7 +1,9 @@
+rm -rf ~/.jim
 mkdir ~/.jim
 mv ./* ~/.jim
 cd ~/.jim
 echo "python ~/.jim/jim.py '\$@'" >> ./jim.sh
 chmod +x jim.sh
+unlink ~/.local/bin/jim
 ln jim.sh ~/.local/bin/jim # change if you want
 echo "Installed. Run 'jim -h'."
