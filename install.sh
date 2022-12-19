6289bc5eb3c1f9dc08aee07c273f3e0f59916db0
jibstack64 <jibbystar@proton.me>
2022-12-19 17:24:29 +0000

    A

--- install.sh
+++ install.sh
@@ -2,7 +2,7 @@ rm -rf ~/.jim
 mkdir ~/.jim
 mv ./* ~/.jim
 cd ~/.jim
-echo "python ~/.jim/jim.py '\$@'" >> ./jim.sh
+echo "python ~/.jim/jim.py \"\"\$@'\"\"" >> ./jim.sh
 chmod +x jim.sh
 unlink ~/.local/bin/jim
 ln jim.sh ~/.local/bin/jim # change if you want
